dfb5a9399d5b3283cfb2599f1c02be661dc1e227
Daniel Byczynski <DanielJByczynski@gmail.com>
2020-07-16 22:44:03 -0500

    Created Create-Submission and Submission-Detail components.

---
+++ src/app/Submission/create-submission/create-submission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-submission',
+  templateUrl: './create-submission.component.html',
+  styleUrls: ['./create-submission.component.scss']
+})
+export class CreateSubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubmissionListComponent } from './Submission/submission-list/submission-list.component';
+import { CreateSubmissionComponent } from './Submission/create-submission/create-submission.component';
+import { SubmissionDetailComponent } from './Submission/submission-detail/submission-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubmissionListComponent
+    SubmissionListComponent,
+    CreateSubmissionComponent,
+    SubmissionDetailComponent
   ],
   imports: [
     BrowserModule,
