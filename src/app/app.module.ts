06955770609fe5ed2589351426e4a2ca1f00ba43
Daniel Byczynski <DanielJByczynski@gmail.com>
2020-07-16 22:41:39 -0500

    Created Submission-List Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubmissionListComponent } from './Submission/submission-list/submission-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmissionListComponent
   ],
   imports: [
     BrowserModule,
