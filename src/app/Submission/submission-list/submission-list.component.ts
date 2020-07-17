005d273a66612a06dcdafe5393aad25e75d3a7b5
Daniel Byczynski <DanielJByczynski@gmail.com>
2020-07-17 01:09:06 -0500

    Added http request for Submissions list.

--- src/app/Submission/submission-list/submission-list.component.ts
+++ src/app/Submission/submission-list/submission-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { Submission } from '../submission';
-
 @Component({
   selector: 'app-submission-list',
   templateUrl: './submission-list.component.html',
@@ -9,10 +10,26 @@ import { Submission } from '../submission';
 })
 export class SubmissionListComponent implements OnInit {
 
-  constructor() { }
+  submissions$: Observable<Submission[]>;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
     console.log("run")
+    this.getSubmissions();
+  }
+
+  options: {
+    headers?: HttpHeaders | {[header: string]: string | string[]},
+    observe?: 'body' | 'events' | 'response',
+    params?: HttpParams|{[param: string]: string | string[]},
+    reportProgress?: boolean,
+    responseType?: 'arraybuffer'|'blob'|'json'|'text',
+    withCredentials?: boolean,
+  }
+
+  getSubmissions() {
+    this.submissions$ = this.http.get<Submission[]>("https://localhost:5001/api/submissions", {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},observe: 'body', responseType: 'json'});
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubmissionListComponent } from './Submission/submission-list/submission-list.component';
-import { CreateSubmissionComponent } from './Submission/create-submission/create-submission.component';
-import { SubmissionDetailComponent } from './Submission/submission-detail/submission-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubmissionListComponent,
-    CreateSubmissionComponent,
-    SubmissionDetailComponent
+    SubmissionListComponent
   ],
   imports: [
     BrowserModule,
